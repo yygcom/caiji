81e34a410490a392d13068e441b3b0e1100f42c1
Anything Liu <hamlzy@gmail.com>
2017-11-14 16:47:34 +0800

    Update tmall.user.js

--- tmall.user.js
+++ tmall.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tmall抓
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
