b4e4beae3d6064cd36fd87629a6833ba0e48a0a2
Anything Liu <hamlzy@gmail.com>
2019-05-27 10:23:39 +0800

    Update milan.user.js

--- milan.user.js
+++ milan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         milanoo抓
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.milanoo.com/fr/produit/*
