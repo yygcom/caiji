f6a374e22b81386a79caf5c45827eb3b17dcc9fa
Anything Liu <hamlzy@gmail.com>
2021-08-06 10:39:26 +0800

    Update taobao抓.user.js

--- taobao抓.user.js
+++ taobao抓.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         taobao抓
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
