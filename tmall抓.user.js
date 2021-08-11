052e1f73f2cbf79471dac4ae09109a6e9637d1df
Anything Liu <hamlzy@gmail.com>
2021-08-11 16:58:58 +0800

    Update tmall抓.user.js

--- tmall抓.user.js
+++ tmall抓.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tmall抓
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.2.5
 // @description  try to take over the world!
 // @author       You
 // @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
