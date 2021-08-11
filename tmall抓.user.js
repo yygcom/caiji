0336d5c48a34583b0d59f9525716365fe0a52933
Anything Liu <hamlzy@gmail.com>
2021-08-11 10:41:45 +0800

    Add files via upload

---
+++ tmall抓.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         tmall抓
+// @namespace    http://tampermonkey.net/
+// @version      0.2.3
+// @description  try to take over the world!
+// @author       You
+// @require      https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js
+// @match        http://detail.tmall.com/item.htm*
+// @match        https://detail.tmall.com/item.htm*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var $$ = jQuery.noConflict();
+    var domain = 'http://www.tmall.com/';
+    var postdomain = 'tmall.com';
+    var serv = '172.20.3.194:3000';
+    var servs = '172.20.3.194:3001';
+
+    var download = function(){
+        $$('#outdiv').css({'width':'500px','height':'500px'});
+        var pid = $$('#LineZing').attr('itemid');
+        $$('#J_UlThumb img').each(function(idx,obj){
+            var imgurl = $$(obj).attr("src").replace(/\/\//,'http://').replace(/_\d+x\d+.*/,'');
+            console.log(imgurl);
+
+            $$('#outdiv').append('<div style="border-bottom: solid 1px #ccc; margin-bottom:5px;">图片<img width="35" height="45" src="' +imgurl+'"/>已经加入下载进程，请注意查看<div>');
+
+            $$.post("https://"+servs+"/getimg",{domain:postdomain,imgurl:imgurl,pid:pid},function(result){
+                //no
+                console.log(result);
+            });
+        });
+        
+        $$('.J_TSaleProp a').each(function(idx,obj){
+            var testxx = $$(obj).attr('style');
+            if(testxx !== undefined){
+                var imgurl = testxx.replace(/background:url\(/,'https:').replace(/_\d+x\d+.*/,'');
+                console.log(imgurl);
+                $$('#outdiv').append('<div style="border-bottom: solid 1px #ccc; margin-bottom:5px;">图片<img width="35" height="45" src="' +imgurl+'"/>已经加入下载进程，请注意查看<div>');
+                $$.post("https://"+servs+"/getimg",{domain:postdomain,imgurl:imgurl,pid:pid},function(result){
+                    console.log(result);
+                });
+            }
+        });
+        
+    };
+
+    $$('body').append('<div id="outdiv" style="border:solid 1px #ccc;background:#eaeaea;padding: 10px;position: absolute;top: 0;right: 0;width: 300px;height: 70px;overflow-y: scroll;z-index:200000001;"></div>');
+
+    $$('#outdiv').append('<div style="height:30px;"><span style="cursor:pointer; border:solid 1px #ccc;background-color:#bababa;color:red;padding:10px;margin-right:10px;" id="getvedio">视频 </span> <span style="cursor:pointer; border:solid 1px #ccc;background-color:#bababa;color:red;padding:10px;margin-right:10px;" id="dlhand">下载此产品的图片</span> <a style="border:solid 1px #ccc;background-color:#999;color:#fff;padding:10px;;" id="showhand" href="http://'+serv+'/yaaw" target="_blank">查看下载进程</a></div>');
+    $$('#outdiv').append('<div style="height:20px;">图片位置 <pre>\\\\172.20.3.194\\wwwroot\\caiji\\images\\ </pre></div>');
+
+    $$('#dlhand').click(function(){
+        //download();
+        ckst();
+    });
+    
+    $$('#getvedio').click(function(){
+        //console.log($$('video.lib-video')[0].currentSrc);
+        var newWeb=window.open('_blank');
+        newWeb.location = $$('video.lib-video')[0].currentSrc;
+    });
+
+    var ckst = function(){
+        //$$.get('https://172.20.3.194:3001/test?t='+Math.rand(),function(data){
+
+        //});
+        $$.get('https://172.20.3.194:3001/test?t='+Math.random(), function(data){
+            //console.log('ok');
+            download();
+        }).fail(function() {
+            window.open('https://172.20.3.194:3001');
+            //alert('woops'); // or whatever
+        });
+    }
+
+    // Your code here...
+})();
