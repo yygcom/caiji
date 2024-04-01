440b1666c2486ac741cce7d263d0e8c9f9deee58
Anything Liu <hamlzy@gmail.com>
2024-04-01 14:25:18 +0800

    Update tb2024.user.js

--- tb2024.user.js
+++ tb2024.user.js
@@ -24,8 +24,9 @@
     var download = function(){
         $$('#outdiv').css({'width':'500px','height':'500px'});
         var pid = $$('#aliww-click-trigger').data("item");
+        console.log($$('#aliww-click-trigger').data("item"));
 
-        
+
         //$$('#J_UlThumb img').each(function(idx,obj){
         //    var imgurl = $$(obj).attr("src").replace(/\/\//,'https://').replace(/_\d+x\d+.*/,'');
         //    console.log(imgurl);
@@ -55,6 +56,11 @@
                 if (imgNode) {
                     // 对 <img> 标签进行操作，比如获取属性值等
                     console.log(imgNode.src.replace(/_\d+x\d+.*/,''));
+                    var imgurl = imgNode.src.replace(/_\d+x\d+.*/,'');
+                    $$('#outdiv').append('<div style="border-bottom: solid 1px #ccc; margin-bottom:5px;">图片<img width="35" height="45" src="' +imgurl+'"/>已经加入下载进程【一组】，请注意查看<div>');
+                    $$.post("https://"+servs+"/getimg",{domain:postdomain,imgurl:imgurl,pid:pid},function(result){
+                        console.log(result);
+                    });
                 }
             });
         }
@@ -86,25 +92,51 @@
         //        console.log(result);
         //    });
         //});
-        // 获取 XPath
-        var xpath = '//*[@id="root"]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[2]';
 
-        // 使用 document.evaluate() 获取节点
-        var result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        scrollToEnd();
+        setTimeout(function() {
+            scrollToTop();
+            // 获取 XPath
+            var xpath2 = '//*[@id="root"]/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/div[2]';
 
-        // 如果找到了节点
-        if (result && result.singleNodeValue) {
-            var divNode = result.singleNodeValue;
-            // 查找 <img> 标签
-            var imgNodes = divNode.querySelectorAll('img');
-            // 遍历 <img> 标签
-            imgNodes.forEach(function(imgNode) {
-                // 对 <img> 标签进行操作，比如获取属性值等
-                console.log(imgNode.src);
-            });
-        }
+            // 使用 document.evaluate() 获取节点
+            var result2 = document.evaluate(xpath2, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+
+            // 如果找到了节点
+            if (result2 && result2.singleNodeValue) {
+                var divNode = result2.singleNodeValue;
+                // 查找 <img> 标签
+                var imgNodes = divNode.querySelectorAll('img');
+                // 遍历 <img> 标签
+                imgNodes.forEach(function(imgNode) {
+                    // 对 <img> 标签进行操作，比如获取属性值等
+                    console.log(imgNode.src);
+                    var imgurl = imgNode.src.replace(/_.webp/,'');
+                    if($$(imgNode).data("src") != ""){
+                        imgurl = $$(imgNode).data("src");
+                    }
+                    if(imgurl != undefined) {
+                        $$('#outdiv').append('<div style="border-bottom: solid 1px #ccc; margin-bottom:5px;">图片<img width="35" height="45" src="' +imgurl+'"/>已2经加入下载进程【描述】，请注意查看<div>');
+                        $$.post("https://"+servs+"/getimg",{domain:postdomain,imgurl:imgurl,pid:pidx},function(result){
+                            console.log(result);
+                        });
+                    }
+
+                });
+            }
+        }, 2300);
 
     };
+
+    // d2022 begin
+    function scrollToEnd(){//滚动到底部
+        var h = 1200;//滚动让起暴露 $$(document).height()-$$(window).height();
+        $$(document).scrollTop(h);
+    }
+    function scrollToTop(){//滚动到顶部
+        var h = 0;
+        $$(document).scrollTop(h);
+    }
 
     $$('body').append('<div id="outdiv" style="border:solid 1px #ccc;background:#eaeaea;padding: 10px;position: absolute;top: 0;right: 0;width: 360px;height: 70px;overflow-y: scroll;z-index:200000001;"></div>');
 
